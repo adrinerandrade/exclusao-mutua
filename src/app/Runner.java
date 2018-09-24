4cfbb91bb1f00a9e8ceeb97ea5bd616aad79d54c
Adriner Andrade <adriner_andrade@hotmail.com>
2018-09-24 00:58:31 -0300

    adicionando exemplificação de criação de serviço

--- src/app/Runner.java
+++ src/app/Runner.java
@@ -1,8 +1,12 @@
 package app;
 
+import impl.ApplicationService;
+import service.Service;
+
 public class Runner {
 
     public static void main(String[] args) {
+        new Service(new ApplicationService(), 3050);
     }
 
 }
