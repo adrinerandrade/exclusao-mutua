6940f70f48af800ae898f16e2d3880dc21ca05fa
Adriner Andrade <adriner_andrade@hotmail.com>
2018-10-05 20:16:49 -0300

    implementação errada

--- src/service/RequestHandlerExtractor.java
+++ src/service/RequestHandlerExtractor.java
@@ -7,7 +7,7 @@ class RequestHandlerExtractor {
     }
 
     static RequestHandler getRequestHandler(Class<? extends ActionHandler> handler) {
-        RequestHandler[] handlerClass = handler.getClass().getDeclaredAnnotationsByType(RequestHandler.class);
+        RequestHandler[] handlerClass = handler.getDeclaredAnnotationsByType(RequestHandler.class);
         if (handlerClass.length != 1) {
             throw new IllegalArgumentException(String.format("Verifique se a classe '%s' possui a anotação '%s'", handler.getCanonicalName(), RequestHandler.class.getCanonicalName()));
         }
